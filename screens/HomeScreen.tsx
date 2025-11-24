bd92dd3f635a34f5577c0aad1b5af31ebfca646a
bahatrk <139872481+bahatrk@users.noreply.github.com>
2025-11-24 20:33:15 +0300

    home screen e search bar emlak ve araba butınu, ikonlar ve bunların konumlandırılması ıcın gereklı seylerı yaptım

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput,TouchableOpacity} from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../navigation/RootNavigator";
 
@@ -17,28 +19,39 @@ export default function HomeScreen({navigation}: Props){
             {/* Arama Barı */}
             <TextInput
                 style={styles.searchBar}
-                placeholder="Araba, ev, eşya ara..."
+                placeholder="Araba, ev ara..."
+                placeholderTextColor={'black'}
             />
 
             {/* Kategoriler Başlığı */}
             <Text style={styles.title}>Kategoriler</Text>
 
-            <View style={styles.categories}>
-                {/* Araba Butonu */}
-                <TouchableOpacity
-                style={styles.categoryButton}
-                onPress={() => navigation.navigate('CarList')}
-                >
-                <Text style={styles.buttonText}>🚗 Araba</Text>
-                </TouchableOpacity>
+            <View style={styles.categoriesContainer}>
+                 <View style={styles.categoryWrapper}>
+                    {/* Emlak Butonu */}
+                    <TouchableOpacity
+                    style={styles.categoryButton}
+                    onPress={() => navigation.navigate('RealEstateList')}
+                    >
+                        <View style={{backgroundColor: 'orange', borderRadius: '50%', padding: 10}}>
+                            <Entypo name="home" size={24} color="white" />
+                        </View>
+                        <Text style={styles.buttonText}>Emlak</Text>
+                    </TouchableOpacity>
+                </View>
 
-                {/* Emlak Butonu */}
-                <TouchableOpacity
-                style={styles.categoryButton}
-                onPress={() => navigation.navigate('RealEstateList')}
-                >
-                <Text style={styles.buttonText}>🏠 Emlak</Text>
-                </TouchableOpacity>
+                <View style={styles.categoryWrapper}>
+                    {/* Araba Butonu */}
+                    <TouchableOpacity
+                    style={styles.categoryButton}
+                    onPress={() => navigation.navigate('CarList')}
+                    >
+                        <View style={{backgroundColor: 'red', borderRadius: '50%', padding: 10}}>
+                            <MaterialCommunityIcons name="steering" size={24} color="white" />
+                        </View>
+                        <Text style={styles.buttonText}>Araba</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         
         </View>
@@ -46,22 +59,31 @@ export default function HomeScreen({navigation}: Props){
 }
 
 const styles = StyleSheet.create({
-    container:{ flex: 1,justifyContent: 'center',alignItems: 'center'},
+    container:{ flex: 1, padding: 20, backgroundColor: '#fff'},
     searchBar: {
         padding: 12,
         borderWidth: 1,
-        borderColor: '#ccc',
         borderRadius: 10,
         marginBottom: 20,
+        width: '100%',
+        backgroundColor: '#D3D3D3',
     },
     title: { fontSize: 20, fontWeight: 'bold', marginBottom: 15 },
-    categories: { flexDirection: 'row', justifyContent: 'space-between' },
-    categoryButton: {
-        backgroundColor: '#ffcc00',
-        paddingVertical: 20,
-        width: '48%',
-        borderRadius: 12,
-        alignItems: 'center',
+    categoriesContainer: { flexDirection: 'column', marginTop: 10},
+    categoryWrapper: {
+        marginBottom: 5, // her buton arasına boşluk
     },
-    buttonText: { fontSize: 18, fontWeight: '600' },
+    categoryButton: {
+        paddingVertical: 20,
+        width: '100%',
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    buttonText: { 
+        fontSize: 18, 
+        fontWeight: '600', 
+        marginLeft: 12,
+    },
 });
