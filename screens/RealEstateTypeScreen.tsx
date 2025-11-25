483cbde49cf08e472cd64ef5b9f9f3f026d5de61
bahatrk <139872481+bahatrk@users.noreply.github.com>
2025-11-25 20:16:52 +0300

    butonların yerlerını degıstım ve ikon ekledım

--- screens/RealEstateTypeScreen.tsx
+++ screens/RealEstateTypeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/RootNavigator';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 type Props = { navigation: StackNavigationProp<RootStackParamList, 'RealEstateType'> };
 
@@ -13,6 +14,7 @@ export default function RealEstateTypeScreen({ navigation }: Props) {
         onPress={() => navigation.navigate('RealEstateSale', { type: 'Konut' })}
       >
         <Text style={styles.text}>Konut</Text>
+        <MaterialCommunityIcons name="greater-than" size={22} color="black" style={styles.icon} />
       </TouchableOpacity>
 
       <TouchableOpacity
@@ -20,13 +22,35 @@ export default function RealEstateTypeScreen({ navigation }: Props) {
         onPress={() => navigation.navigate('RealEstateSale', { type: 'Bina' })}
       >
         <Text style={styles.text}>Bina</Text>
+        <MaterialCommunityIcons name="greater-than" size={22} color="black" style={styles.icon}/>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' },
-  button: { width: '80%', padding: 20, backgroundColor: 'orange', marginVertical: 15, borderRadius: 10, alignItems: 'center' },
-  text: { color: 'white', fontSize: 20, fontWeight: 'bold' },
+  container: { 
+    flex: 1, 
+    justifyContent: 'flex-start', 
+    alignItems: 'center', 
+    backgroundColor: 'white', 
+  },
+  button: { 
+      paddingVertical: 20,
+      width: '100%',
+      borderBottomWidth: 1,
+      borderBottomColor: '#ccc',
+      flexDirection: 'row',
+  },
+  text: { 
+    color: 'black', 
+    fontSize: 20, 
+    marginLeft: 20,
+  },
+  icon: {
+    marginTop: 5,
+    marginLeft: 'auto',
+    marginRight: 10,
+    color: "#c8c8c8",
+  },
 });
