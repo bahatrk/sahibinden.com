d0a093ffb4719e204fc918355f2e930dd763789e
bahatrk <139872481+bahatrk@users.noreply.github.com>
2025-11-28 19:49:42 +0300

    arabaların bır nesnesını olusturup markası logosu modelini iceren bır nesne olusturdum

---
+++ screens/CarScreens/CarData.ts
@@ -0,0 +1,110 @@
+export const carData = [
+    {
+        brand: "Audi",
+        logo: "https://i.pinimg.com/736x/2d/46/f3/2d46f364e5a5edabc69f6dd4ab0c4e54.jpg",
+        otomobilModels: ["A3", "A4", "A6"],
+        suvModels: ["Q3", "Q5", "Q7"]
+    },
+    {
+        brand: "BMW",
+        logo: "https://i.pinimg.com/736x/d5/2d/bb/d52dbb929ad9353d39537f01a60fa6f0.jpg",
+        otomobilModels: ["1 Serisi", "2 Serisi", "3 Serisi", "4 Serisi", "5 Serisi", "6 Serisi", "7 Serisi"],
+        suvModels: ["X1", "X2", "X3", "X4", "X5", "X6", "X7"]
+    },
+    {
+        brand: "Citroen",
+        logo: "https://i.pinimg.com/1200x/b5/9a/c1/b59ac1f7264a8c0be96b65fb6ea634f2.jpg",
+        otomobilModels: ["C-Elysee", "C1", "C2", "C3", "C4", "C5"],
+        suvModels: ["C3 AirCross", "C4 Cactus", "C5 Aircross"]
+    },
+    {
+        brand: "Dodge",
+        logo: "https://i.pinimg.com/1200x/ef/a3/18/efa318decff6823a12b1cae5ac81fb60.jpg",
+        otomobilModels: ["Avenger", "Challenger", "Viper", "Charger", "Magnum"],
+        suvModels: ["Durango", "Journey", "Ram", "Caliber", "Nitro"]
+    },
+    {
+        brand: "Ford",
+        logo: "https://i.pinimg.com/1200x/31/15/8b/31158b7fd6b665dc137abf4d6700e3d6.jpg",
+        otomobilModels: ["Fiesta", "Focus", "Fusion", "Mondeo"],
+        suvModels: ["Kuga", "Puma", "Ranger", "EcoSport"]
+    },
+    {
+        brand: "Honda",
+        logo: "https://i.pinimg.com/1200x/fa/49/dc/fa49dcaf6ae263e7e796590c6c610cd4.jpg",
+        otomobilModels: ["Accord", "City", "Civic", "Jazz"],
+        suvModels: ["CR-V", "HR-V", "ZR-V"]
+    },
+    {
+        brand: "Hyundai",
+        logo: "https://i.pinimg.com/736x/48/ea/3e/48ea3e86df65c9bab9f3a961ad1f1df1.jpg",
+        otomobilModels: ["Accent", "Elantra", "i10", "i20", "i30"],
+        suvModels: ["Bayon", "ix35", "Kona", "Tucson"]
+    },
+    {
+        brand: "Kia",
+        logo: "https://i.pinimg.com/1200x/98/67/c7/9867c7ce44d8b9d20f4d85829488c8b0.jpg",
+        otomobilModels: ["Ceed", "Picanto", "Rio"],
+        suvModels: ["Sorento", "Sportage", "Stonic", "XCeed"]
+    },
+    {
+        brand: "Mercedes-Benz",
+        logo: "https://i.pinimg.com/736x/f2/0b/a6/f20ba65164b6fc224d036ad519cb6a09.jpg",
+        otomobilModels: ["A Serisi", "CLA", "E Serisi", "EQE", "S Serisi"],
+        suvModels: ["EQB", "G Serisi", "GLA", "GLC", "ML"]
+    },
+    {
+        brand: "Nissan",
+        logo: "https://i.pinimg.com/1200x/93/c5/0c/93c50c059576b9282b1ebd33edcd7ea0.jpg",
+        otomobilModels: ["Almera", "Micra", "Primera"],
+        suvModels: ["Juke", "Qashqai", "X-Trail"]
+    },
+        {
+        brand: "Opel",
+        logo: "https://i.pinimg.com/1200x/6a/2e/21/6a2e21f4fc09ffb8b299e73fee23bd97.jpg",
+        otomobilModels: ["Astra", "Corsa", "Insignia", "Vectra"],
+        suvModels: ["Crossland", "Grandland", "Mokka"]
+    },
+        {
+        brand: "Peugeot",
+        logo: "https://i.pinimg.com/1200x/2b/04/fd/2b04fd2b361abae98dfe64662450d882.jpg",
+        otomobilModels: ["206", "308", "508"],
+        suvModels: ["2008", "3008", "5008"]
+    },
+        {
+        brand: "Porsche",
+        logo: "https://i.pinimg.com/736x/b2/88/83/b28883eebed8dc930f92e82587eb1ec7.jpg",
+        otomobilModels: ["911", "Panamera", "Taycan"],
+        suvModels: ["Cayenne", "Cayenne Coupe", "Macan"]
+    },
+        {
+        brand: "Renault",
+        logo: "https://i.pinimg.com/736x/09/32/2b/09322bb85b0311254f995390b2d99d27.jpg",
+        otomobilModels: ["Clio", "Fluence", "Megane", "Talisman"],
+        suvModels: ["Austral", "Duster", "Captur", "Kadjar", "Koleos"]
+    },
+        {
+        brand: "Skoda",
+        logo: "https://i.pinimg.com/1200x/a5/2e/88/a52e882d1d45f22b7be7b00e75b2552e.jpg",
+        otomobilModels: ["Fabia", "Octavia", "Superb"],
+        suvModels: ["Kamiq", "Karoq", "Kodiaq"]
+    },
+        {
+        brand: "TOGG",
+        logo: "https://i.pinimg.com/1200x/8c/b9/1c/8cb91cf8315e946a695aa7f9adbbf9e0.jpg",
+        otomobilModels: ["T10F"],
+        suvModels: ["T10X"]
+    },
+        {
+        brand: "Toyota",
+        logo: "https://i.pinimg.com/1200x/5c/94/92/5c94927a6199a01174a919f04b060f87.jpg",
+        otomobilModels: ["Auris", "Corolla", "Yaris"],
+        suvModels: ["C-HR", "Hilux", "RAV4"]
+    },
+        {
+        brand: "Volkswagen",
+        logo: "https://i.pinimg.com/736x/74/3c/59/743c59530f840b351e235a426195f964.jpg",
+        otomobilModels: ["Golf", "Jetta", "Passat", "Polo"],
+        suvModels: ["Amarok", "T-Cross", "T-Roc", "Tiguan"]
+    },
+];
