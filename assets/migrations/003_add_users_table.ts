9de42ea65742bce41e9ca7cbea9530444ab2c2ac
bahatrk <139872481+bahatrk@users.noreply.github.com>
2025-12-10 18:36:05 +0300

    eskı yapılanlar cok statıktı ve bende onları degıstırıp dınamık hale getırdım

--- assets/migrations/003_add_users_table.ts
+++
@@ -1,9 +0,0 @@
-export const addusersTableSQL = `
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    ad TEXT,  
-    soyad TEXT,
-    email TEXT UNIQUE, 
-    sifre TEXT
-);
-`;
--- type/types.ts
+++
@@ -1,5 +0,0 @@
-// types.ts
-export type RootStackParamList = {
-  Categories: { parentId: number | null; parentName?: string };
-  ListingPage: { categoryId: number; categoryName: string };
-};
